ea27193a67f453784a0c2b820975f229fb1f14d5
Aditya Bharti <119076885+Bharti2002aditya@users.noreply.github.com>
2024-02-04 02:15:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// let iceCream = "chocolate";
+// if (iceCream === "chocolate") {
+//   alert("Yay, I love chocolate ice cream!");
+// } else {
+//   alert("Awwww, but chocolate is my favorite…");
+// }
+// let myImage = document.querySelector("img");
+
+// myImage.onclick = () => {
+//   let mySrc = myImage.getAttribute("src");
+//   if (mySrc === "img/firefox1") {
+//     myImage.setAttribute("src", "img/firefox2");
+//   } else{
+//     myImage.setAttribute("src", "img/firefox-1");
+//   }
+// };
+
+var input = document.getElementById('age');
+
+var button = document.getElementById('tes')
+
+
+button.addEventListener("click", function (num1) {
+    var x = prompt("ENTER YOUR AGE", "0");
+    var num1 = parseInt(x);
+   
+    
+    var wholeLife = 100 -num1; 
+    var week = 52*wholeLife;
+   var days =365*wholeLife;
+    var months = 12*wholeLife;
+alert( "In your life" +" " +days+ " " +"DAYS"+ " ," +week+ " " + " WEEKS" + " ," +months+ " " +"MONTHS"+  " "+"are left.")
+alert("THANK YOU FOR USING OUR WEBSITE , HOW WAS YOUR EXPRIENCE SAHARE ON THIS NUMBERR 91+7024099675");
+
+
+  });
+
+
+
+//   var submitBtn = document.getElementById('tes');
+//   var newSubmit = submitBtn*10;
+//   var wholeLife = 100 - newSubmit; 
+
+// newSubmit.addEventListener("click", function(newSubmit) {
+  
+//     // var wholeLife = 100 - newSubmit; 
+//     var week = 52*wholeLife;
+//     var days =365*wholeLife;
+//     var months = 12*wholeLife;
+//     alert(newSubmit)
+// alert( "In your life" +" " +days+ " " +"DAYS"+ " ," +week+ " " + " WEEKS" + " ," +months+ " " +"MONTHS"+  " "+"are left.")
+
+        
+    
+//   });
+
